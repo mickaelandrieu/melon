430766339db7663649075a0bcb31652c4fed9c33
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2021-10-29 01:24:08 +0200

    Set minimal version to PS 1.7.8

--- melon.php
+++ melon.php
@@ -15,6 +15,6 @@ class Melon extends Module
         $this->displayName = $this->trans('Melon', [], 'Modules.Melon.Admin');
         $this->description = $this->trans('une description', [], 'Modules.Melon.Admin');
 
-        $this->ps_versions_compliancy = array('min' => '1.7.5', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = ['min' => '1.7.8', 'max' => _PS_VERSION_];
     }
 }
